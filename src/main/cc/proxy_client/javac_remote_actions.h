59cccb4e16156e6e96d2ce517150c476329110b2
Ramy Medhat <abdelaal@google.com>
2019-04-23 15:19:24 -0400

    Find inputs in command and allow out directory for RUN_ALL_REMOTELY commands

--- src/main/cc/proxy_client/javac_remote_actions.h
+++ src/main/cc/proxy_client/javac_remote_actions.h
@@ -1,24 +1,9 @@
 #ifndef REMOTE_CLIENT_JAVAC_REMOTE_ACTIONS_H_
 #define REMOTE_CLIENT_JAVAC_REMOTE_ACTIONS_H_
 
-#include <set>
-#include <string>
-
 namespace remote_client {
-
-using std::set;
-using std::string;
-
 // IsJavacAction determines if the given action is a java compile action.
 bool IsJavacAction(int argc, char** argv);
-
-// FindAllFilesFromCommand splits each of the given args by space and colon,
-// checks if each individual piece of command is a file and if it is, appends
-// the file to the given set.
-// This is useful to find "hidden inputs" i.e., inputs not explicitly specified
-// to rbecc (from Ninja), but those that the command depends on for it to be
-// successfully remotely executed.
-void FindAllFilesFromCommand(int argc, char** argv, set<string>* files);
 }
 
 #endif  // REMOTE_CLIENT_JAVAC_REMOTE_ACTIONS_H_
--- src/main/cc/proxy_client/proxy_client.cc
+++ src/main/cc/proxy_client/proxy_client.cc
@@ -141,6 +141,61 @@ string GetCompilerDir(const char* compiler) {
   return compiler_dir;
 }
 
+string Trim(const string& cmd) {
+  if (cmd.length() == 0) {
+    return "";
+  }
+
+  size_t st_idx = 0;
+  size_t ed_idx = cmd.length() - 1;
+
+  while (st_idx <= ed_idx && !absl::ascii_isalpha(cmd[st_idx])) ++st_idx;
+  while (ed_idx > st_idx && !absl::ascii_isalpha(cmd[ed_idx])) --ed_idx;
+
+  return cmd.substr(st_idx, ed_idx - st_idx + 1);
+}
+
+bool IsFile(const string& name) {
+  struct stat buffer;   
+  return stat(name.c_str(), &buffer) == 0;
+}
+
+void FindFiles(const string& cmd, set<string>* files) {
+  if (cmd.find(" ") != string::npos) {
+    for (const auto& c : absl::StrSplit(cmd, ' ', absl::SkipEmpty())) {
+      FindFiles(string(c), files);
+    }
+    return;
+  }
+  if (cmd.find(":") != string::npos) {
+    for (const auto& c : absl::StrSplit(cmd, ':', absl::SkipEmpty())) {
+      FindFiles(string(c), files);
+    }
+    return;
+  }
+
+  const string trimmed_cmd = Trim(cmd);
+  if (IsFile(trimmed_cmd)) {
+    files->insert(trimmed_cmd);
+  }
+}
+
+// FindAllFilesFromCommand splits each of the given args by space and colon,
+// checks if each individual piece of command is a file and if it is, appends
+// the file to the given set.
+// This is useful to find "hidden inputs" i.e., inputs not explicitly specified
+// to rbecc (from Ninja), but those that the command depends on for it to be
+// successfully remotely executed.
+void FindAllFilesFromCommand(int argc, char** argv, set<string>* files) {
+  if (files == nullptr) {
+    return;
+  }
+
+  for (int i = 0; i < argc; ++i) {
+    FindFiles(string(argv[i]), files);
+  }
+}
+
 int GetInputsFromIncludeProcessor(const string& cmd_id, int argc, char** argv, const char** env,
                                   const string& cwd, set<string>* inputs) {
   const char* server_address = getenv("INCLUDE_SERVER_ADDRESS");
@@ -327,6 +382,9 @@ int ComputeInputs(int argc, char** argv, const char** env, const string& cwd, co
     use_args_inputs = true;
     inputs->insert("prebuilts/jdk/jdk9/linux-x86");
     inputs->insert("external/icu");
+    FindAllFilesFromCommand(argc, argv, inputs);
+  } else if (getenv("RUN_ALL_REMOTELY")) {
+    use_args_inputs = true;
     FindAllFilesFromCommand(argc, argv, inputs);
   }
 
